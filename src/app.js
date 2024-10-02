6874789ab19edff1b7a62082eb5ee008540784da
chefdude99 <chefdude.1999@gmail.com>
2024-10-02 19:46:35 +0000

    Generador de dominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big", "small"];
+  let noun = ["jogger", "racoon"];
+  let exten = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let c = 0; c < exten.length; c++) {
+          console.log(pronoun[i] + adj[a] + noun[b] + exten[c]);
+        }
+      }
+    }
+  }
 };
